0fa297821794293e05d852df7f1aa6f6aa163146
JPCard <44414304+JPCard@users.noreply.github.com>
2020-04-01 16:30:53 -0300

    Delete IVistaReceptor.java

--- Emisor/receptor/vista/IVistaReceptor.java
+++
@@ -1,9 +0,0 @@
-package receptor.vista;
-
-import emisor.modelo.Mensaje;
-
-public interface IVistaReceptor {
-    public void activarAlerta();
-    public void mostrarMensaje(Mensaje mensaje);
-    public void mostrarErrorNoReceptor();
-}
